03fb11c74e3033c7bd29f6536f69c70489ddabb9
Laurel Laoang, Jr <laurellaoang@gmail.com>
2024-06-12 09:21:08 +0200

    Initial Commit

---
+++ Dad Jokes/Dad Jokes/Dad_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dad_JokesApp.swift
+//  Dad Jokes
+//
+//  Created by Laurel Laoang, Jr on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Dad_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
